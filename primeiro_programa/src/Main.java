f0ab581a864f3e83566c4c785c69b89a48df2821
Lucas Teles <lucasouzateles@gmail.com>
2024-03-13 11:20:45 -0300

    adicionando novo exercicio

--- primeiro_programa/src/Main.java
+++ primeiro_programa/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+    String nome = "Marcos";
+    int idade = 45;
+    double renda = 4142.23;
 
-        System.out.println("Hello world!");
+    System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 
     }
 }
