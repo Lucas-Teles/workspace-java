cc7ed018a25370d7eabe79e08d2b16bd075c2746
Lucas Teles <lucasouzateles@gmail.com>
2024-03-13 16:38:25 -0300

    adicionando novos exercicios

--- fixacao_04/src/Main.java
+++ fixacao_04/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number < 0) {
+            System.out.println("NEGATIVO");
+        } else if (number == 0) {
+            System.out.println("NAO NEGATIVO");
+        } else {
+            System.out.println("NAO NEGATIVO");
+        }
     }
 }
